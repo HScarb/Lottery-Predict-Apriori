d975789ec97a23fcc1afd758008006d0a90dd5fd
Scarb <jjhfen00@163.com>
2016-11-08 12:04:32 +0800

    finish my apriori

--- Scarb_apriori.py
+++ Scarb_apriori.py
@@ -1,6 +1,8 @@
 # read lottery.txt
 import sys
 import copy
+import itertools
+import operator
 
 
 # try:
@@ -115,8 +117,28 @@ class Apriori(object):
         self.min_sup_val = min_sup
 
     def apriori(self):
-        None
+        Lkm1 = self.find_frequent_1_itemsets()
+        L = Lkm1
 
+        while Lkm1 != []:
+            c_dic = {}
+            Ck = self.apriori_gen(Lkm1)
+            for datalist in self.data:
+                for item in Ck:
+                    if set(item) <= set(dataList):      # 如果datalist包含itemn，C[item]++
+                        if tuple(item) in c_dic:
+                            c_dic[tuple(item)] +=1
+                        else:
+                            c_dic[tuple(item)] = 1
+
+            Lk = []
+            for item in c_dic:
+                if c_dic[item] >= self.min_sup_val:
+                    Lk.append([item])
+
+            Lkm1 = Lk
+            L += Lk
+        return L
 
     def find_frequent_1_itemsets(self):
         """
@@ -133,11 +155,44 @@ class Apriori(object):
                 freq_list.append([item])
         return freq_list
 
-    def apriori_gen(self):
-        None
+    def apriori_gen(self, Lkm1):
+        """
+        由L(k-1)生成C(k)
+        """
+        Ck = []
+        k = len(Lkm1[0] + 1)
+        for l1 in Lkm1:
+            for l2 in Lkm1:
+                flag = False
+                # 比较除了最后一项，如果不同，则跳出
+                for i in range(k - 2):
+                    if l1[i] != l2[i]:
+                        flag = True
+                        break
+                if flag: continue
+                # 当最后一项 l1 < l2 时，l1与l2最后一项合并
+                if l1[k - 2] < l2[k - 2]:
+                    c = l1 + [l2[k - 2]]
+                else:
+                    continue
+                # 将没有频繁项集的项排除
+                if self.has_infrequent_subset(c, Lkm1):
+                    continue
+                else:
+                    Ck.append(c)
+        return Ck
 
-    def has_infrequent_subset(self):
-        None
+    def has_infrequent_subset(self, Ck, Lkm1):
+        """
+        返回C(k)的某一项的所有(k-1)项集中是否有非频繁项集
+        """
+        k = len(Lkm1[0] + 1)
+        subsets = [itertools.combinations(Ck, k - 1)]
+        for item in subsets:
+            item = [item]
+            if item not in Lkm1:
+                return True
+        return False
 
 if __name__ == '__main__':
     filePath = 'ssq.TXT'
@@ -145,3 +200,5 @@ if __name__ == '__main__':
     data = [data[2:8] for data in dataList[1:]]
     t = Apriori(data, 2)
     t.find_frequent_1_itemsets()
+    for i in range(5):
+        print(i)
